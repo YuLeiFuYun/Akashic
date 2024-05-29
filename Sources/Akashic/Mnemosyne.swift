ccf4c85af74b0494f5de03b948b25b1f091c8b9c
玉垒浮云 <yuleifuyunn@gmail.com>
2024-05-29 12:02:33 +0800

    Remove DoublyLinkedList and refactor Mnemosyne cache management
    - Deleted `DoublyLinkedList.swift` file
    - Refactored `Mnemosyne` class to remove dependency on `DoublyLinkedList`
    - Replaced `LRUCacheNode` with `CacheNode`
    - Updated protocols and class properties to reflect new structure
    - Added `CostCalculable` protocol and extended `UIImage` to conform to it
    - Implemented new methods for cache handling and memory management
    - Improved notification handling for app lifecycle events

--- Sources/Akashic/Mnemosyne.swift
+++ Sources/Akashic/Mnemosyne.swift
@@ -5,36 +5,41 @@
 //  Created by 玉垒浮云 on 2024/4/30.
 //
 
-import Foundation
+import UIKit
 
-public protocol CacheCostCalculable: CustomStringConvertible {
-    var cacheCost: Int { get }
+public protocol CostCalculable {
+    var cost: Int { get }
 }
 
-final class LRUCacheNode<Tag: Hashable, Payload: CacheCostCalculable>: NodeProtocol {
-    let tag: Tag
-    var payload: Payload
+extension UIImage: CostCalculable {
+    public var cost: Int {
+        guard let cgImage else { return 0 }
+        
+        let bytesPerFrame = cgImage.bytesPerRow * cgImage.height
+        let frameCount = images == nil ? 1 : images!.count
+        return bytesPerFrame * frameCount
+    }
+}
+
+fileprivate final class CacheNode<Key: Hashable, Value: CostCalculable> {
+    let key: Key
+    var value: Value
     var clock: Int
     var isActive = false
-    var isToBeDeleted = false
-    var expirationDate: Date?
-    var isExpired: Bool { Date() > expirationDate! }
+    var expirationDate: CFTimeInterval = 0
+    var isExpired: Bool { CACurrentMediaTime() > expirationDate }
     
-    var prev: LRUCacheNode<Tag, Payload>?
-    var next: LRUCacheNode<Tag, Payload>?
+    unowned(unsafe) var prev: CacheNode<Key, Value>?
+    unowned(unsafe) var next: CacheNode<Key, Value>?
     
-    var description: String {
-        ""
-    }
-
-    init(tag: Tag, payload: Payload, clock: Int) {
-        self.tag = tag
-        self.payload = payload
+    init(key: Key, value: Value, clock: Int) {
+        self.key = key
+        self.value = value
         self.clock = clock
     }
 }
 
-public final class Mnemosyne<Key: Hashable, Value: CacheCostCalculable>: CustomStringConvertible, @unchecked Sendable {
+public final class Mnemosyne<Key: Hashable, Value: CostCalculable>: @unchecked Sendable {
     public struct Config {
         // 最大内存成本限制
         var costLimit: Int
@@ -46,13 +51,15 @@ public final class Mnemosyne<Key: Hashable, Value: CacheCostCalculable>: CustomS
         var ttl: TimeInterval
         // 过期缓存清理间隔
         var cleanupInterval: TimeInterval
+        // 程序进入后台时是否要保留缓存，默认 false，即不保留。
+        var keepWhenEnteringBackground: Bool = false
         
         public init(
             costLimit: Int = Int(ProcessInfo.processInfo.physicalMemory) / 4,
             entryCostLimit: Double = 0.1,
             countLimit: Int = .max,
-            ttl: TimeInterval = 120,
-            cleanupInterval: TimeInterval = 120
+            ttl: TimeInterval = 60,
+            cleanupInterval: TimeInterval = 60
         ) {
             self.costLimit = costLimit
             self.entryCostLimit = entryCostLimit
@@ -63,18 +70,17 @@ public final class Mnemosyne<Key: Hashable, Value: CacheCostCalculable>: CustomS
     }
     
     private let config: Config
-    private var cacheMap = [Key: LRUCacheNode<Key, Value>]()
-    private var fifoQueue = FIFOQueue<LRUCacheNode<Key, Value>>()
-    private var lruList = DoublyLinkedList<Key, Value, LRUCacheNode<Key, Value>>()
+    private var cacheMap = [Key: CacheNode<Key, Value>]()
+    private var bufferQueueKeys = FIFOQueue<Key>()
     private var lruListKeys: Set<Key> = []
+    private unowned(unsafe) var head: CacheNode<Key, Value>?
+    private unowned(unsafe) var tail: CacheNode<Key, Value>?
     private var totalCost = 0
     private var totalCount = 0
     private var clock = 0
     private let lock: os_unfair_lock_t
     private var memoryPressureSource: DispatchSourceMemoryPressure?
     private var timer: Timer!
-    
-    public var description: String { lruList.description }
     
     public init(config: Config = .init()) {
         self.config = config
@@ -92,6 +98,14 @@ public final class Mnemosyne<Key: Hashable, Value: CacheCostCalculable>: CustomS
             self?.removeAllValues()
         }
         memoryPressureSource?.resume()
+        
+        let notifications: [(Notification.Name, Selector)] = [
+            (UIApplication.willTerminateNotification, #selector(clearCacheIfNeeded)),
+            (UIApplication.didEnterBackgroundNotification, #selector(clearCacheIfNeeded))
+        ]
+        notifications.forEach {
+            NotificationCenter.default.addObserver(self, selector: $0.1, name: $0.0, object: nil)
+        }
     }
 
     deinit {
@@ -102,53 +116,6 @@ public final class Mnemosyne<Key: Hashable, Value: CacheCostCalculable>: CustomS
         memoryPressureSource?.cancel()
     }
     
-    public func setValue(_ value: Value, forKey key: Key) {
-        os_unfair_lock_lock(lock)
-        defer { os_unfair_lock_unlock(lock) }
-
-        guard Double(value.cacheCost) < config.entryCostLimit * Double(config.costLimit) else { return }
-        
-        if let existingNode = cacheMap[key] {
-            if lruListKeys.contains(key) {
-                lruList.delete(existingNode)
-                totalCost -= existingNode.payload.cacheCost
-                existingNode.payload = value
-                existingNode.expirationDate = Date().addingTimeInterval(config.ttl)
-                totalCost += value.cacheCost
-                trimIfNeeded()
-                lruList.append(existingNode)
-            } else {
-                existingNode.isActive = true
-                totalCost -= existingNode.payload.cacheCost
-                existingNode.payload = value
-                totalCost += value.cacheCost
-                trimIfNeeded()
-            }
-        } else {
-            let node = LRUCacheNode(tag: key, payload: value, clock: clock)
-            cacheMap[key] = node
-            totalCost += value.cacheCost
-            totalCount += 1
-            trimIfNeeded()
-            fifoQueue.enqueue(node)
-        }
-    }
-    
-    public func value(forKey key: Key) -> Value? {
-        os_unfair_lock_lock(lock)
-        defer { os_unfair_lock_unlock(lock) }
-
-        guard let node = cacheMap[key] else { return nil }
-        
-        if node.isActive, lruListKeys.contains(key) {
-            lruList.delete(node)
-            node.expirationDate = Date().addingTimeInterval(config.ttl)
-            lruList.append(node)
-        }
-        node.isActive = true
-        return node.payload
-    }
-    
     public func isCached(forKey key: Key) -> Bool {
         os_unfair_lock_lock(lock)
         defer { os_unfair_lock_unlock(lock) }
@@ -156,12 +123,82 @@ public final class Mnemosyne<Key: Hashable, Value: CacheCostCalculable>: CustomS
         return cacheMap[key] != nil
     }
     
+    public func setValue(_ value: Value, forKey key: Key) {
+        os_unfair_lock_lock(lock)
+        defer { os_unfair_lock_unlock(lock) }
+        
+        guard Double(value.cost) < config.entryCostLimit * Double(config.costLimit) else { return }
+        if let existingNode = cacheMap[key] {
+            if lruListKeys.contains(existingNode.key) {
+                deleteNode(existingNode)
+                totalCost -= existingNode.value.cost
+                existingNode.value = value
+                existingNode.expirationDate = CACurrentMediaTime() + config.ttl
+                totalCost += value.cost
+                trimIfNeeded()
+                appendNode(existingNode)
+            } else {
+                existingNode.isActive = true
+                totalCost -= existingNode.value.cost
+                existingNode.value = value
+                totalCost += value.cost
+                trimIfNeeded()
+            }
+        } else {
+            let node = CacheNode(key: key, value: value, clock: clock)
+            cacheMap[key] = node
+            totalCost += value.cost
+            totalCount += 1
+            trimIfNeeded()
+            bufferQueueKeys.enqueue(key)
+        }
+    }
+    
+    public func value(forKey key: Key) -> Value? {
+        os_unfair_lock_lock(lock)
+        defer { os_unfair_lock_unlock(lock) }
+        
+        guard let node = cacheMap[key] else { return nil }
+        if lruListKeys.contains(node.key) {
+            deleteNode(node)
+            node.expirationDate = CACurrentMediaTime() + config.ttl
+            appendNode(node)
+        }
+        
+        node.isActive = true
+        return node.value
+    }
+    
     @discardableResult
     public func removeValue(forKey key: Key) -> Value? {
         os_unfair_lock_lock(lock)
         defer { os_unfair_lock_unlock(lock) }
 
-        return _removeValue(forKey: key)
+        guard let node = cacheMap[key] else { return nil }
+        return _remove(node)
+    }
+    
+    public func removeExpiredValues() {
+        os_unfair_lock_lock(lock)
+        defer { os_unfair_lock_unlock(lock) }
+        
+        while let key = bufferQueueKeys.peek {
+            guard let node = cacheMap[key] else {
+                bufferQueueKeys.dequeue()
+                continue
+            }
+            
+            if node.clock < clock {
+                bufferQueueKeys.dequeue()
+                handleDequeuedNode(forKey: key)
+            } else {
+                break
+            }
+        }
+        
+        while let node = head, node.isExpired {
+            _remove(node)
+        }
     }
     
     public func removeAllValues() {
@@ -170,64 +207,65 @@ public final class Mnemosyne<Key: Hashable, Value: CacheCostCalculable>: CustomS
 
         totalCost = 0
         totalCount = 0
-        cacheMap.removeAll()
-        lruList = DoublyLinkedList<Key, Value, LRUCacheNode<Key, Value>>()
+        head = nil
+        tail = nil
         lruListKeys.removeAll()
-        fifoQueue.removeAll()
+        bufferQueueKeys.removeAll()
+        cacheMap.removeAll()
     }
     
-    public func removeExpiredValues() {
-        os_unfair_lock_lock(lock)
-        defer { os_unfair_lock_unlock(lock) }
-        
-        while let node = fifoQueue.peek, node.clock < clock {
-            fifoQueue.dequeue()
-            handleDequeuedFIFONode(node: node)
+    @objc private func clearCacheIfNeeded() {
+        guard !config.keepWhenEnteringBackground else { return }
+        removeAllValues()
+    }
+    
+    private func trimIfNeeded() {
+        while totalCost > config.costLimit || totalCount > config.countLimit, let key = bufferQueueKeys.dequeue() {
+            handleDequeuedNode(forKey: key)
         }
         
-        while let node = lruList.head, node.isExpired {
-            _removeValue(forKey: node.tag)
+        while totalCost > config.costLimit || totalCount > config.countLimit, let node = head {
+            _remove(node)
         }
     }
     
     @discardableResult
-    private func _removeValue(forKey key: Key) -> Value? {
-        guard let node = cacheMap[key] else { return nil }
+    private func _remove(_ node: CacheNode<Key, Value>) -> Value? {
+        totalCost -= node.value.cost
+        totalCount -= 1
+        deleteNode(node)
+        lruListKeys.remove(node.key)
+        cacheMap[node.key] = nil
         
-        if lruListKeys.contains(key) {
-            totalCost -= node.payload.cacheCost
-            totalCount -= 1
-            lruList.delete(node)
-            lruListKeys.remove(key)
-        } else {
-            node.isToBeDeleted = true
-        }
-        
-        cacheMap[key] = nil
-        return node.payload
+        return node.value
     }
     
-    private func trimIfNeeded() {
-        guard totalCost > config.costLimit || totalCount > config.countLimit else { return }
-        
-        while totalCost > config.costLimit || totalCount > config.countLimit, let node = fifoQueue.dequeue() {
-            handleDequeuedFIFONode(node: node)
-        }
-        
-        while totalCost > config.costLimit || totalCount > config.countLimit, let node = lruList.head {
-            _removeValue(forKey: node.tag)
-        }
+    private func appendNode(_ node: CacheNode<Key, Value>) {
+        if head == nil { head = node }
+        node.prev = tail
+        tail?.next = node
+        tail = node
     }
     
-    private func handleDequeuedFIFONode(node: LRUCacheNode<Key, Value>) {
-        if node.isActive, !node.isToBeDeleted {
-            node.expirationDate = Date().addingTimeInterval(config.ttl)
-            lruList.append(node)
-            lruListKeys.insert(node.tag)
+    private func deleteNode(_ node: CacheNode<Key, Value>) {
+        if head === node { head = node.next }
+        if tail === node { tail = node.prev }
+        node.next?.prev = node.prev
+        node.prev?.next = node.next
+        node.next = nil
+    }
+    
+    private func handleDequeuedNode(forKey key: Key) {
+        guard let node = cacheMap[key] else { return }
+        
+        if node.isActive {
+            node.expirationDate = CACurrentMediaTime() + config.ttl
+            appendNode(node)
+            lruListKeys.insert(node.key)
         } else {
-            totalCost -= node.payload.cacheCost
+            totalCost -= node.value.cost
             totalCount -= 1
-            cacheMap[node.tag] = nil
+            cacheMap[node.key] = nil
         }
     }
 }
